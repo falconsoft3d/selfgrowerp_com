ac69cd6f05f1c675baf6cd63654ec9ac620430ea
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2025-04-04 12:10:13 +0200

    sc

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
             </div>
             <div className="flex items-center justify-center">
               <Image
-                src="/placeholder.svg?height=400&width=400"
+                src="/developer.png?height=400&width=400"
                 alt="SelfGrowerP Dashboard"
                 width={400}
                 height={400}
